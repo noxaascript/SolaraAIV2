af8314d31e1706dd847f96e5f193c2e55f0dc778
noxaascript <bagasprivasi@gmail.com>
2026-06-21 00:06:17 +0700

    Update groq.py

--- providers/groq.py
+++ providers/groq.py
@@ -1,19 +1,22 @@
 import requests
+from config import GROQ_API_KEY
 
 
-def ask_groq(api_key, model, prompt):
+def ask_groq(model, prompt):
+
+    if not GROQ_API_KEY:
+        return "No GROQ_API_KEY in config"
 
     url = "https://api.groq.com/openai/v1/chat/completions"
 
     headers = {
-        "Authorization": f"Bearer {api_key}",
+        "Authorization": f"Bearer {GROQ_API_KEY}",
         "Content-Type": "application/json"
     }
 
     payload = {
         "model": model,
-        "messages": [{"role": "user", "content": prompt}],
-        "temperature": 0.7
+        "messages": [{"role": "user", "content": prompt}]
     }
 
     res = requests.post(url, json=payload, headers=headers)

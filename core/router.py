52d124f950e368e9e97b69a61895ff822c316f83
noxaascript <bagasprivasi@gmail.com>
2026-06-20 16:48:16 +0700

    Update router.py

--- core/router.py
+++ core/router.py
@@ -1,28 +1,31 @@
 from core.plugins import run_plugin
 from core.tools import run_tool
-from core.memory import auto_learn, get_memory
+from core.memory import auto_learn, get_memory, normalize
 from providers.groq import ask_groq
 
 
 def route(user_input):
 
-    # 🧠 auto learn memory dulu
+    # 🧠 normalize dulu (slang fix)
+    user_input = normalize(user_input)
+
+    # 🧠 auto learn
     learned = auto_learn(user_input)
     if learned:
         return learned
 
-    # 🔌 plugin system (@echo hello)
+    # 🔌 plugin system
     if user_input.startswith("@"):
         parts = user_input[1:].split(" ", 1)
         name = parts[0]
         text = parts[1] if len(parts) > 1 else ""
         return run_plugin(name, text)
 
-    # ⚙️ tools (/ls /pwd /run)
+    # ⚙️ tools
     if user_input.startswith("/"):
         return run_tool(user_input)
 
-    # 🤖 AI MODE
+    # 🤖 AI mode
     name = get_memory("name")
 
     context = ""

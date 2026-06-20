91a8a0ad73fe6a906c4de6bdd4db252479f0c520
noxaascript <bagasprivasi@gmail.com>
2026-06-20 23:47:50 +0700

    Update website_pipeline.py

--- core/website_pipeline.py
+++ core/website_pipeline.py
@@ -1,6 +1,6 @@
-from core.web_generator import generate_website
-from core.web_memory import save_web_project, save_to_main_file
 from core.workspace import auto_app
+from core.web_memory import save_web_project, save_to_main_file
+from core.web_generator import generate_website
 
 
 def create_web_project(name, prompt):
@@ -8,7 +8,6 @@ def create_web_project(name, prompt):
 
     save_web_project(name, prompt, files)
     save_to_main_file(name, prompt, files)
-
     auto_app(name, prompt)
 
     return "File Web tersimpan di WebMemory/web.txt"

2b3f5183e073fdd21a5d3c56eca144e3a664344f
noxaascript <bagasprivasi@gmail.com>
2026-06-21 09:45:46 +0700

    Update memory.py

--- model_core/memory.py
+++ model_core/memory.py
@@ -1,18 +1,31 @@
 memory_store = []
 
 
-def add_memory(user, prompt, response):
+def add_memory(user, prompt, response, score=1):
 
     memory_store.append({
         "user": user,
         "prompt": prompt,
-        "response": response
+        "response": response,
+        "score": score
     })
 
-    if len(memory_store) > 50:
+    if len(memory_store) > 100:
         memory_store.pop(0)
 
 
 def get_memory(limit=5):
 
-    return memory_store[-limit:]
+    sorted_mem = sorted(
+        memory_store,
+        key=lambda x: x["score"],
+        reverse=True
+    )
+
+    return sorted_mem[:limit]
+
+
+def boost_memory(index):
+
+    if index < len(memory_store):
+        memory_store[index]["score"] += 1

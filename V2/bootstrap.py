ca6923ba55c68dc251ddd40b6e1e3ddb5ea6ef65
noxaascript <bagasprivasi@gmail.com>
2026-06-21 18:57:26 +0700

    Refactor boot function to improve logging

--- V2/bootstrap.py
+++ V2/bootstrap.py
@@ -1,5 +1,6 @@
-def boot(project_folder, main_file, project_files):
-    print("[BOOT] starting system...")
+from V2.orchestrator.system_router import start_system
 
-    from V2.orchestrator.system_router import start_system
+def boot(project_folder, main_file, project_files):
+    print("[BOOT] active")
+
     return start_system(project_folder, main_file, project_files)

2b26b5052c89d22d916cb1e1e805279f313087e7
noxaascript <bagasprivasi@gmail.com>
2026-06-21 15:51:05 +0700

    Refactor scan_project to simplify file handling

--- autofixer/scanner.py
+++ autofixer/scanner.py
@@ -1,26 +1,19 @@
 import os
 
 def scan_project(folder):
-    files_data = []
+    files = []
 
-    for root, _, files in os.walk(folder):
-        for f in files:
+    for root, _, fns in os.walk(folder):
+        for f in fns:
             if f.endswith(".py"):
                 path = os.path.join(root, f)
 
-                try:
-                    with open(path, "r", encoding="utf-8") as file:
-                        content = file.read()
+                with open(path, "r", encoding="utf-8") as file:
+                    content = file.read()
 
-                    files_data.append({
-                        "file": path,
-                        "content": content
-                    })
+                files.append({
+                    "path": path,
+                    "content": content
+                })
 
-                except Exception as e:
-                    files_data.append({
-                        "file": path,
-                        "content": f"ERROR READ FILE: {str(e)}"
-                    })
-
-    return files_data
+    return files

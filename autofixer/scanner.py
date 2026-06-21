8ed3eaa1cb6d72077775512533fd68bd408d24d8
noxaascript <bagasprivasi@gmail.com>
2026-06-21 18:57:05 +0700

    Update scanner.py

--- autofixer/scanner.py
+++ autofixer/scanner.py
@@ -3,10 +3,9 @@ import os
 def scan_project(project_folder):
     files = []
 
-    for root, dirs, filenames in os.walk(project_folder):
+    for root, _, filenames in os.walk(project_folder):
         for f in filenames:
-            path = os.path.join(root, f)
-            files.append(path)
+            files.append(os.path.join(root, f))
 
-    print(f"[SCANNER] found {len(files)} files")
+    print(f"[SCANNER] {len(files)} files found")
     return files

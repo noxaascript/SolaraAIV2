0a417ad34c111db7d66011f16ba83ffb7e78c367
noxaascript <bagasprivasi@gmail.com>
2026-06-21 18:39:33 +0700

    Enhance scan_project to list all files in directory

--- autofixer/scanner.py
+++ autofixer/scanner.py
@@ -1,3 +1,12 @@
+import os
+
 def scan_project(project_folder):
-    print(f"[SCANNER] scanning: {project_folder}")
-    return []
+    files = []
+
+    for root, dirs, filenames in os.walk(project_folder):
+        for f in filenames:
+            path = os.path.join(root, f)
+            files.append(path)
+
+    print(f"[SCANNER] found {len(files)} files")
+    return files

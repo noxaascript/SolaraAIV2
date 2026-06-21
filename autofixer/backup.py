ab659d1b9b890ef15e5f0fd6fd2861dea7dc0512
noxaascript <bagasprivasi@gmail.com>
2026-06-21 16:18:44 +0700

    Enhance backup_file to save backups with timestamps
    Refactor backup_file function to create timestamped backups in a designated directory.

--- autofixer/backup.py
+++ autofixer/backup.py
@@ -1,10 +1,21 @@
 import shutil
+import os
 import time
 
+BACKUP_DIR = "AutoFixer/backups"
+
 def backup_file(path):
-    try:
-        backup_path = path + ".bak_" + str(int(time.time()))
-        shutil.copy(path, backup_path)
-        return backup_path
-    except:
-        return None
+    if not os.path.exists(BACKUP_DIR):
+        os.makedirs(BACKUP_DIR)
+
+    if not os.path.exists(path):
+        return
+
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+    filename = path.replace("/", "_")
+
+    backup_path = f"{BACKUP_DIR}/{filename}_{timestamp}.bak"
+
+    shutil.copy2(path, backup_path)
+
+    return backup_path

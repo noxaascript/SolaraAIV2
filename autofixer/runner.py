4bf5cb28fa58b8326524b0d9e3254c6c84ccf71f
noxaascript <bagasprivasi@gmail.com>
2026-06-21 16:17:07 +0700

    Update and rename rumner.py to runner.py

--- autofixer/runner.py
+++ autofixer/runner.py
@@ -1,10 +1,10 @@
 import subprocess
 
 def run_file(file):
-    result = subprocess.run(
+    p = subprocess.run(
         ["python", file],
         capture_output=True,
         text=True
     )
 
-    return result.returncode, result.stdout, result.stderr
+    return p.returncode, p.stdout, p.stderr

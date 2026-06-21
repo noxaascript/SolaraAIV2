0df811ba206ff0a98f1281d8c7b222cb49b34f22
noxaascript <bagasprivasi@gmail.com>
2026-06-21 15:52:25 +0700

    Update main.py

--- autofixer/main.py
+++ autofixer/main.py
@@ -1,25 +1,38 @@
 import sys
+from utils import print_banner
+from scanner import scan_project
 from runner import run_file
-from debugger import debug_error
+from ai import analyze_and_fix
+from patcher import apply_patch
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: python main.py target.py")
+    print_banner()
+
+    if len(sys.argv) < 3:
+        print("Usage: python main.py <project_folder> <main_file>")
         return
 
-    file = sys.argv[1]
+    folder = sys.argv[1]
+    file = sys.argv[2]
 
+    print("🔍 scanning project...")
+    files = scan_project(folder)
+
+    print("🚀 running program...\n")
     code, out, err = run_file(file)
 
     if code == 0:
-        print("✅ OK RUN")
+        print("OK")
         print(out)
-    else:
-        print("❌ ERROR DETECTED\n")
-        print(err)
+        return
 
-        print("\n🤖 AI FIX:\n")
-        print(debug_error(err))
+    print("❌ ERROR DETECTED\n")
+    print(err)
+
+    print("\n🧠 AI ANALYZING + FIXING...\n")
+    ai_result = analyze_and_fix(err, files)
+
+    apply_patch(ai_result)
 
 
 if __name__ == "__main__":

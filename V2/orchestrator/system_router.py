8a60bc2958fbe34ed10f16885e17349ffd20c0ac
noxaascript <bagasprivasi@gmail.com>
2026-06-21 16:09:04 +0700

    Update system_router.py

--- V2/orchestrator/system_router.py
+++ V2/orchestrator/system_router.py
@@ -1,23 +1,22 @@
-from Core import router
-from V2.runtime.error_hook import handle_error
 from V2.runtime.executor_bridge import run_core
+from V2.runtime.error_hook import handle_error
+from V2.plugins.plugin_loader import load_plugins
 
 def start_system():
     print("[V2] System Router Online")
 
+    load_plugins()
+
     while True:
         try:
-            user_input = input("[Solara V2] $ ")
+            user = input("[Solara V2] $ ")
 
-            if user_input.lower() in ["exit", "quit"]:
-                print("shutting down...")
+            if user.lower() in ["exit", "quit"]:
+                print("bye 👋")
                 break
 
-            # kirim ke core system lama
-            result = run_core(user_input)
-
+            result = run_core(user)
             print(result)
 
         except Exception as e:
-            print("[V2 ERROR DETECTED]")
             handle_error(str(e))

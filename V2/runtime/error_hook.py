88c90a7a85620f91161abe3ae34127e06c020774
noxaascript <bagasprivasi@gmail.com>
2026-06-21 16:10:46 +0700

    Update error_hook.py

--- V2/runtime/error_hook.py
+++ V2/runtime/error_hook.py
@@ -1,10 +1,9 @@
 from AutoFixer.main import main as auto_fix
 
 def handle_error(error_text):
-    print("[V2] sending to AutoFixer...")
+    print("[V2] Error detected → AutoFixer running...")
 
     try:
         auto_fix()
-
     except Exception as e:
         print("[AutoFixer FAILED]", str(e))

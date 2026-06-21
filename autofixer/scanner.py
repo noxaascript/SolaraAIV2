4fb78c72c68e3ee2d4427182a5e638446ed6ead0
noxaascript <bagasprivasi@gmail.com>
2026-06-21 18:23:39 +0700

    Update scanner.py

--- autofixer/scanner.py
+++ autofixer/scanner.py
@@ -1,17 +1,2 @@
-import os
-
-def scan_project(folder):
-    files = []
-
-    for root, _, fns in os.walk(folder):
-        for f in fns:
-            if f.endswith(".py"):
-                path = os.path.join(root, f)
-
-                with open(path, "r", encoding="utf-8") as file:
-                    files.append({
-                        "path": path,
-                        "content": file.read()
-                    })
-
-    return files
+def scan_project():
+    print("[SCANNER] project OK")

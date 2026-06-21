dfa0c316739e703db5dd7eb4651935fe538bb0a4
noxaascript <bagasprivasi@gmail.com>
2026-06-21 15:59:42 +0700

    Update patcher.py

--- autofixer/patcher.py
+++ autofixer/patcher.py
@@ -1,3 +1,5 @@
+from backup import backup_file
+
 def apply_patch(output):
     blocks = output.split("FILE:")
 
@@ -9,10 +11,14 @@ def apply_patch(output):
             path = b.split("\n")[0].strip()
             code = b.split("CODE:")[1].strip()
 
+            # 🔥 BACKUP DULU
+            backup_file(path)
+
+            # lalu patch
             with open(path, "w", encoding="utf-8") as f:
                 f.write(code)
 
-            print("FIXED:", path)
+            print("✔ FIXED:", path)
 
         except Exception as e:
             print("PATCH ERROR:", e)

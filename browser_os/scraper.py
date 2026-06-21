27ce27b03b2132b13282417b8194147eb36b974b
noxaascript <bagasprivasi@gmail.com>
2026-06-21 09:32:25 +0700

    Update scraper.py

--- browser_os/scraper.py
+++ browser_os/scraper.py
@@ -1,1 +1,50 @@
+from bs4 import BeautifulSoup
+from browser_os.browser import browser
 
+
+def get_title():
+
+    html = browser.get_html()
+
+    if not html:
+        return "No page opened"
+
+
+    soup = BeautifulSoup(
+        html,
+        "html.parser"
+    )
+
+
+    if soup.title:
+        return soup.title.text
+
+
+    return "No title"
+
+
+def get_text(limit=5000):
+
+    html = browser.get_html()
+
+    if not html:
+        return "No page opened"
+
+
+    soup = BeautifulSoup(
+        html,
+        "html.parser"
+    )
+
+
+    for tag in soup(
+        ["script", "style"]
+    ):
+        tag.decompose()
+
+
+    text = soup.get_text(
+        separator="\n"
+    )
+
+    return text[:limit]
